4305aa1b57d80ef297b1b93bd4511088571c94cc
Varis Nijat <vnijat93@gmail.com>
2024-11-10 14:07:51 -0500

    DEV Use livemodel

--- app/src/main/java/com/example/myquiz/QuizViewModel.kt
+++ app/src/main/java/com/example/myquiz/QuizViewModel.kt
@@ -1,22 +1,32 @@
 package com.example.myquiz
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
 import kotlin.random.Random
 
 class QuizViewModel: ViewModel() {
-    val number1 = MutableLiveData<Int>(null)
-    val number2 = MutableLiveData<Int>(null)
-    val totalCount = MutableLiveData<Int>(0)
-    val showSolution = MutableLiveData<Boolean>(false)
-    val hasPlayed = MutableLiveData<Boolean>(false)
+    private val _number1 = MutableLiveData<Int>(null)
+    val number1: LiveData<Int> get() = _number1
+
+    private val _number2 = MutableLiveData<Int>(null)
+    val number2: LiveData<Int> get() = _number2
+
+    private val _totalCount = MutableLiveData<Int>(0)
+    val totalCount: LiveData<Int> get() = _totalCount
+
+    private val _showSolution = MutableLiveData<Boolean>(false)
+    val showSolution: LiveData<Boolean> get() = _showSolution
+
+    private val _hasPlayed = MutableLiveData<Boolean>(false)
+    val hasPlayed: LiveData<Boolean> get() = _hasPlayed
 
     fun generateNumbers() {
-        number1.value = Random.nextInt(0, 101)
-        number2.value = Random.nextInt(0, 101)
-        totalCount.value = totalCount.value?.plus(1)
-        showSolution.value = false
-        hasPlayed.value = true
+        _number1.value = Random.nextInt(0, 101)
+        _number2.value = Random.nextInt(0, 101)
+        _totalCount.value = totalCount.value?.plus(1)
+        _showSolution.value = false
+        _hasPlayed.value = true
     }
 
     fun areNumbersNull(): Boolean {
@@ -25,8 +35,8 @@ class QuizViewModel: ViewModel() {
 
     fun solveButtonClicked() {
         if (!areNumbersNull()){
-            showSolution.value = true
-            hasPlayed.value = true
+            _showSolution.value = true
+            _hasPlayed.value = true
         }
     }
 }
